f787328e4057f279c94f9e3110c89457a7a0f50a
abdelalibakka <pakkatatata@gmail.com>
2023-11-17 14:02:49 -0800

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,22 @@
-# Handle the requests failling
+# Increase the ULIMIT for Nginx
+file { '/etc/default/nginx':
+  ensure  => file,
+  content => template('path/to/nginx_config.erb'),
+  require => Exec['nginx_config_change'],
+}
 
-# Increase the ULIMIT
-
-exec { 'fix--for-nginx':
+exec { 'nginx_config_change':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin'
+  path    => ['/usr/local/bin', '/bin'],
+  notify  => Exec['nginx_restart'],
 }
 
-# restart Nginx
-
-exec { 'nginx-restart':
+# Restart Nginx if the configuration has changed
+exec { 'nginx_restart':
   command => '/etc/init.d/nginx restart',
-  path    => '/etc/init.d/'
+  path    => '/etc/init.d/',
+  refreshonly => true,
 }
+
+# Usage of template('path/to/nginx_config.erb') assumes that you have an ERB template
+# at 'path/to/nginx_config.erb' that contains the desired content for the nginx config.
