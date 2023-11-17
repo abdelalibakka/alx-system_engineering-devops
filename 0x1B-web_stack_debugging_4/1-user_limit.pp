f4c70516fff97c292f241e09f2105d1726315ef4
abdelalibakka <pakkatatata@gmail.com>
2023-11-17 14:11:43 -0800

    update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,14 @@
 # Enable the user holberton
 
-# Increase hard file limit
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+# Increase file limits for holberton user using augeas
+augeas { 'holberton_file_limits':
+  context => '/files/etc/security/limits.conf',
+  changes => [
+    'set holberton hard nofile 50000',
+    'set holberton soft nofile 50000',
+  ],
+  onlyif  => 'match holberton[. = "hard" and ./nofile = "5"] size > 0 or match holberton[. = "soft" and ./nofile = "4"] size > 0',
 }
 
-# Increase soft file limit
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
-}
+# This ensures that the user holberton exists before making changes
+User['holberton'] -> Augeas['holberton_file_limits']
