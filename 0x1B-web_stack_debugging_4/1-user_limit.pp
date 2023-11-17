2ed3348dffef3975e77772d7717c74c9eb7678f3
abdelalibakka <pakkatatata@gmail.com>
2023-11-17 14:13:50 -0800

    update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -7,7 +7,7 @@ augeas { 'holberton_file_limits':
     'set holberton hard nofile 50000',
     'set holberton soft nofile 50000',
   ],
-  onlyif  => 'match holberton[. = "hard" and ./nofile = "5"] size > 0 or match holberton[. = "soft" and ./nofile = "4"] size > 0',
+  onlyif  => 'match user[. = "holberton"] size > 0 and (match user[. = "holberton"]/nofile[. = "5"] size > 0 or match user[. = "holberton"]/nofile[. = "4"] size > 0)',
 }
 
 # This ensures that the user holberton exists before making changes
